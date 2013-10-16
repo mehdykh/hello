520a668cbf7e06f5505f092c88f785489dd78767
Mehdy Khayyami <mehdykh1982@gmail.com>
2013-10-16 10:50:15 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+This is the README file.
+========================
+
+Descirbe the project so all can install and download it.
---
+++ hello.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>Hello world</title>
+</head>
+<body>
+<h1>Hello</h1>
+<?php echo "My name is Mehdi" ?>
+</body>
+</html>
